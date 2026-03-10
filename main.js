414653ad765328717be9a74d52e9b0752d6b2262
Perfect03 <zlobin_gg52926@mail.ru>
2026-03-11 00:22:38 +0500

    update imports

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import {Lfstrm} from 'lifestream-api'
+import {Lfstrm} from 'LifeStream-API'
 
 const KT = new Lfstrm({DOMAIN_API: 'kt', DOMAIN_FRONT: 'tv.telecom.kz'})
 export default KT.azotExtension
